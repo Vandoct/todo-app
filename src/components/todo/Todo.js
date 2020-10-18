d9aeeac76163454d6d324649bcdef4fec1ef927c
vandoc <ichvandiocta@gmail.com>
2020-10-18 21:52:27 +0700

    Change done, cancel button to checkbox

--- src/components/todo/Todo.js
+++ src/components/todo/Todo.js
@@ -2,25 +2,19 @@ import React, { useContext } from 'react'
 import { AppContext } from '../../context/AppContext'
 
 const Todo = ({ item }) => {
-	const { handleDone, handleCancel, handleRemove } = useContext(AppContext)
+	const { handleToggle, handleRemove } = useContext(AppContext)
 
 	return (
 		<div className="w-full max-w-4xl mx-auto">
 			<div className="bg-gray-700 text-white hover:bg-gray-600 text-xl font-semibold rounded-lg p-5">
 				<div className="grid grid-cols-12">
-					<p className={`col-span-10 ${(item.complete) ? 'line-through' : ''}`}>{item.todo}</p>
-					<div className="grid grid-flow-col gap-x-3">
-						<span
-							className={`col-span-6 mx-auto my-auto text-base ${(item.complete) ? 'text-yellow-500 hover:text-yellow-700' : 'text-teal-500 hover:text-teal-700'} cursor-pointer`}
-							onClick={() => {
-								if (item.complete) {
-									handleCancel(item.id)
-								} else {
-									handleDone(item.id)
-								}
-							}}>
-							{(item.complete) ? 'Cancel' : 'Done'}
-						</span>
+					<input
+						className="col-span-1 self-center"
+						type="checkbox"
+						checked={item.complete}
+						onChange={() => handleToggle(item.id, !item.complete)} />
+					<p className={`col-span-10 ${(item.complete) ? 'line-through text-gray-400' : ''}`}>{item.todo}</p>
+					<div className="col-span-1">
 						<span
 							className="col-span-6 mx-auto my-auto text-base text-red-700 hover:text-red-900 cursor-pointer"
 							onClick={() => handleRemove(item.id)}>
